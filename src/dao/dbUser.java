9a388d8fee1013306c94592d6265e9f78cccd7dd
dcfox2847 <dcfox2847@gmail.com>
2020-07-07 03:56:58 -0400

    -Finished the logger class, in order to add data about login activity to a log file.

---
+++ src/dao/dbUser.java
@@ -0,0 +1,10 @@
+package dao;
+
+import java.sql.*;
+import model.User;
+
+public class dbUser {
+
+
+
+}
--- src/utils/Log.java
+++ src/utils/Log.java
@@ -1,8 +1,28 @@
 package utils;
 
+import java.io.*;
+import java.time.ZonedDateTime;
+
 public class Log {
 
-    // Create the functions needed to log information to a text file here.
-    // To be used with the 'Login' view and controller.
+    // Class variables go here
+   private static final String LOGTITLE = "log" + ZonedDateTime.now();
+
+
+    // Class Constructors go here
+    public Log() {};
+
+    // Class methods go here
+
+    public static void writeLog(int userID, String username, boolean success) {
+        try (FileWriter fileWriter = new FileWriter(LOGTITLE, true);
+                BufferedWriter bufferedWriter = new BufferedWriter(fileWriter);
+                PrintWriter printWriter = new PrintWriter(bufferedWriter);){
+            printWriter.println("User ID: "+ userID + " , User Name: " + username + " , Time: " + ZonedDateTime.now()
+            + " , Login Successful: " + (success ? " True" : " False"));
+        }catch (IOException ex){
+        System.out.println("Error logged: " + ex.getMessage());
+    }
+    }
 
 }
