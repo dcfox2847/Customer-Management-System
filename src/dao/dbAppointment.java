a3633b5d9f84593107a02274b6531db11a01069d
dcfox2847 <dcfox2847@gmail.com>
2020-07-12 16:28:18 -0400

    -Added the function to get weekly appointments to the dbAppointment class. - TODO: WORK ON THE FXML CONTROLS TO TEST THE PROPER DISPLAY OF DATA

--- src/dao/dbAppointment.java
+++ src/dao/dbAppointment.java
@@ -2,9 +2,13 @@ package dao;
 
 import java.sql.*;
 import java.time.*;
+import java.util.Locale;
+
 import javafx.collections.*;
 import dao.dbConnection;
 import model.Appointment;
+
+import static dao.dbConnection.stmt;
 
 public class dbAppointment {
 
@@ -16,7 +20,7 @@ public class dbAppointment {
         try{
             String monthlyQuery = "SELECT * FROM appointment WHERE customerId = '" + id + "'AND " + "start >= '" + begin
                     + "' AND start <= '" + end + "'";
-            ResultSet monthlyResults = dbConnection.stmt.executeQuery(monthlyQuery);
+            ResultSet monthlyResults = stmt.executeQuery(monthlyQuery);
             while(monthlyResults.next()){
                 apt = new Appointment(monthlyResults.getInt("appointmentId"), monthlyResults.getInt("customerId"),
                         monthlyResults.getString("start"), monthlyResults.getString("end"),
@@ -24,11 +28,38 @@ public class dbAppointment {
                         monthlyResults.getString("location"), monthlyResults.getString("contact"));
             }
             System.out.println("Appointments for the month have been returned!");
+            stmt.close();
             return appointments;
         }catch (SQLException ex){
-            System.out.println("SQLException: " + ex.getMessage());
+            System.out.println("SQLException: (monthly query)" + ex.getMessage());
             return null;
         }
     }
+
+    public static ObservableList<Appointment> getWeeklyApt(int id){
+        ObservableList<Appointment> appointments = FXCollections.observableArrayList();
+        Appointment apt;
+        LocalDate begin = LocalDate.now();
+        LocalDate end = LocalDate.now().plusWeeks(1);
+        try{
+            String weeklyQuery = "SELECT * from appointment WHERE customerId = '" + id + "'AND " + "start >= '" + begin + "' AND start <= '" + end + "'";
+            ResultSet weeklyResults = stmt.executeQuery(weeklyQuery);
+            while(weeklyResults.next()) {
+                apt = new Appointment(weeklyResults.getInt("appointmentId"), weeklyResults.getInt("customerId"),
+                        weeklyResults.getString("start"), weeklyResults.getString("end"),
+                        weeklyResults.getString("title"), weeklyResults.getString("description"),
+                        weeklyResults.getString("location"), weeklyResults.getString("contact"));
+                appointments.add(apt);
+            }
+            System.out.println("Appointments for the week have been returned!");
+            stmt.close();
+            return appointments;
+        }catch (SQLException ex) {
+            System.out.println("SQLException (weekly query): " + ex.getMessage());
+            return null;
+        }
+    }
+
+
 
 }
